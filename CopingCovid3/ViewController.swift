b17bb1f0af54813d75d7519ea10074c005ddf208
Kayla <kaygarrett18@yahoo.com>
2020-07-30 13:06:27 -0700

    Initial Commit

---
+++ CopingCovid3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CopingCovid3
+//
+//  Created by Apple on 7/30/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
